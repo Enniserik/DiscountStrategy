ff62cacf658711f3aeaa168480ab73e54f372f49
Erik <Erik@ErikPC>
2014-09-27 16:11:14 -0500

    Created most classes and the DiscountStrategy interface.

---
+++ src/discountstrategy/FakeDatabase.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package discountstrategy;
+
+/**
+ *
+ * @author Erik
+ */
+public class FakeDatabase {
+    
+    // Array of customers
+    private Customer[] customers = {new Customer()};
+    
+    // Array of products
+    private Product[] products = {new Product()};
+    
+}
---
+++ src/discountstrategy/Receipt.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package discountstrategy;
+
+/**
+ *
+ * @author Erik
+ */
+public class Receipt {
+ 
+    // Puts together line items and formats output
+    // Sends output to output object
+    
+}
